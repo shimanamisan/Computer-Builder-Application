52161e555709acc15c42a2383a2b67b7a5074771
shimanamisan <mikan.sup.all@gmail.com>
2022-12-17 07:32:06 +0900

    update: ストレージの項目の処理完了

--- src/js/Controller/StrageSectionController.js
+++ src/js/Controller/StrageSectionController.js
@@ -1,9 +1,14 @@
 import GetApiData from '../Model/GetApiData';
 import StorageViews from '../Viwes/StorageViews';
-import StrageSize from '../ValueObject/StrageSize';
+import ExtractStrageSize from '../ValueObject/ExtractStrageSize';
 import InputChange from './EventController/InputChange';
+import ExtractBrandFromSize from '../ValueObject/ExtractBrandFromSize';
+import ExtractStrageModel from '../ValueObject/ExtractStrageModel';
 
 class StrageSectionController {
+	/**
+   *
+   */
 	static async strageTypeElement() {
 		const hddType = await GetApiData.execution('hdd');
 		const ssdType = await GetApiData.execution('ssd');
@@ -17,50 +22,83 @@ class StrageSectionController {
 
 		const strageTypeLists = Object.keys(hashMap);
 
-		const element = document.getElementById(StorageViews.storageTypeId);
+		const strageTypeEle = document.getElementById(StorageViews.storageTypeId);
 
-		element.innerHTML = `<option selected value="-">-</option>`;
+		strageTypeEle.innerHTML = `<option selected value="-">-</option>`;
 		for (let i = 0; i < strageTypeLists.length; i++) {
-			element.innerHTML += `<option value="${strageTypeLists[i]}">${strageTypeLists[i]}</option>`;
+			strageTypeEle.innerHTML += `<option value="${strageTypeLists[i]}">${strageTypeLists[i]}</option>`;
 		}
 
 		InputChange.addEvent(document.getElementById(StorageViews.storageTypeId), StrageSectionController.strageSizeElements);
 	}
 
+	/**
+   *
+   * @returns
+   */
 	static async strageSizeElements() {
-		const element = document.getElementById(StorageViews.storageSizeId);
+		const strageSizeEle = document.getElementById(StorageViews.storageSizeId);
 		const strageType = document.getElementById(StorageViews.storageTypeId).value.toLowerCase();
 
 		if (strageType === '-') {
-			element.innerHTML = `<option selected value="-">-</option>`;
+			strageSizeEle.innerHTML = `<option selected value="-">-</option>`;
 			return;
 		}
 
 		const apiData = await GetApiData.execution(strageType);
 
 		// ストレージのサイズの文字列を切り出した配列を格納したValueObject（ソート済み）
-		const strageSizeValueObject = new StrageSize(apiData);
+		const strageSizeValueObject = new ExtractStrageSize(apiData);
+		const strageSizeLists = strageSizeValueObject.getStrageSize();
 
-		const sizeLists = strageSizeValueObject.getStrageSize();
-
-		element.innerHTML = `<option selected value="-">-</option>`;
-		for (let i = 0; i < sizeLists.length; i++) {
-			element.innerHTML += `<option value="${sizeLists[i]}">${sizeLists[i]}</option>`;
+		strageSizeEle.innerHTML = `<option selected value="-">-</option>`;
+		for (let i = 0; i < strageSizeLists.length; i++) {
+			strageSizeEle.innerHTML += `<option value="${strageSizeLists[i]}">${strageSizeLists[i]}</option>`;
 		}
 
-		InputChange.addEvent(element, StrageSectionController.strageBrandElement);
+		InputChange.addEvent(strageSizeEle, StrageSectionController.addStrageBrandElement);
 	}
 
-	static async strageBrandElement() {
-		const element = document.getElementById(StorageViews.storageBrandId);
-		const strageSize = document.getElementById(StorageViews.storageBrandId).value.toLowerCase();
+	/**
+   * ストレージのサイズが選択されたらBrand要素を追加する
+   * @returns
+   */
+	static async addStrageBrandElement() {
+		const strageType = document.getElementById(StorageViews.storageTypeId).value.toLowerCase();
+		const strageSize = document.getElementById(StorageViews.storageSizeId).value;
+		const strageBrandEle = document.getElementById(StorageViews.storageBrandId);
 
-		if (strageType === '-') {
-			element.innerHTML = `<option selected value="-">-</option>`;
-			return;
+		const apiData = await GetApiData.execution(strageType);
+
+		const brandValueObject = new ExtractBrandFromSize(strageSize, apiData);
+		const strageBrandLists = brandValueObject.getBrands();
+
+		strageBrandEle.innerHTML = `<option selected value="-">-</option>`;
+		for (let i = 0; i < strageBrandLists.length; i++) {
+			strageBrandEle.innerHTML += `<option value="${strageBrandLists[i]}">${strageBrandLists[i]}</option>`;
 		}
 
-		console.log('change!');
+		InputChange.addEvent(strageBrandEle, StrageSectionController.addStargeModelElement);
+	}
+
+	/**
+   *
+   */
+	static async addStargeModelElement() {
+		const strageType = document.getElementById(StorageViews.storageTypeId).value.toLowerCase();
+		const strageSize = document.getElementById(StorageViews.storageSizeId).value;
+		const strageBrand = document.getElementById(StorageViews.storageBrandId).value;
+		const strageModelEle = document.getElementById(StorageViews.storageModelId);
+
+		const apiData = await GetApiData.execution(strageType);
+		const modelValueObject = new ExtractStrageModel(strageSize, strageBrand, apiData);
+
+		const strageModelLists = modelValueObject.getModel();
+
+		strageModelEle.innerHTML = `<option selected value="-">-</option>`;
+		for (let i = 0; i < strageModelLists.length; i++) {
+			strageModelEle.innerHTML += `<option value="${strageModelLists[i].Model}">${strageModelLists[i].Model}</option>`;
+		}
 	}
 }
 
