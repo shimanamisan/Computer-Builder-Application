e423621307895132553d305d4eef6c17156590f9
shimanamisan <mikan.sup.all@gmail.com>
2023-01-08 14:13:46 +0900

    add: 型定義ファイルを追加

---
+++ src/@types/apiData.d.ts
@@ -0,0 +1,8 @@
+type apiData = {
+  Type: string;
+  'Part Number': string;
+  Brand: string;
+  Model: string;
+  Rank: number;
+  Benchmark: number;
+};
---
+++ src/@types/swiper.d.ts
@@ -0,0 +1,4 @@
+// INFO: https://typescript-jp.gitbook.io/deep-dive/type-system/intro
+// アンビエント宣言（アンビエントモジュール (Ambient Modules)）
+// 既存の JavaScript ライブラリに型情報を付与することが出来る
+declare module 'swiper';
