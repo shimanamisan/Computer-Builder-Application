bdcd8a23a2112f1ee05bd666266b19307e2648ca
shimanamisan <mikan.sup.all@gmail.com>
2023-01-08 23:06:41 +0900

    update: タイポを修正

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,16 +1,16 @@
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
-import '@scss/style';
+import '@scss/style.scss';
 // import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import CpuViews from './Viwes/CpuViews';
-import GpuViews from './Viwes/GpuViews';
-import MemoryViews from './Viwes/MemoryViews';
-import StorageViews from './Viwes/StorageViews';
-import ButtonViews from './Viwes/ButtonViews';
+import CpuViews from './Views/CpuViews';
+import GpuViews from './Views/GpuViews';
+import MemoryViews from './Views/MemoryViews';
+import StorageViews from './Views/StorageViews';
+import ButtonViews from './Views/ButtonViews';
 import InputChange from './Controller/EventController/InputChange';
 import ButtonClick from './Controller/EventController/ButtonClick';
 import StorageSectionController from './Controller/StorageSectionController';
@@ -18,18 +18,12 @@ import MemorySectionController from './Controller/MemorySectionController';
 import CreateComputerController from './Controller/CreateComputerController';
 import CpuSectionController from './Controller/CpuSectionController';
 import GpuSectionConttoller from './Controller/GpuSectionConttoller';
-import CpuEntity from './Entity/CpuEntity';
-import GpuEntity from './Entity/GpuEntity';
-import MemoryEntity from './Entity/MemoryEntity';
-import StorageEntity from './Entity/StorageEntity';
 
 class App {
-  static async createTopView() {
-    // window.CpuEntity = new CpuEntity();
-    // window.GpuEntity = new GpuEntity();
-    // window.MemoryEntity = new MemoryEntity();
-    // window.StorageEntity = new StorageEntity();
-
+  /**
+   * アプリケーションを起動させる
+   */
+  static async createTopView(): Promise<void> {
     const container: HTMLElement = document.createElement('div');
     container.classList.add('col-md-8', 'd-flex', 'flex-column', 'm-auto');
     container.innerHTML += CpuViews.createStringHTML();
@@ -38,34 +32,40 @@ class App {
     container.innerHTML += StorageViews.createStringHTML();
     container.innerHTML += ButtonViews.createStringHTML();
 
-    const target: any | null = document.getElementById('target');
+    // INFO: https://stackoverflow.com/questions/55588968/type-error-object-is-possibly-null-ts2531-for-window-document/55589638
+    // document.getElementByIdがnullを返すかもしれないので !演算子をつけて回避する
+    const target: HTMLElement = document.getElementById('target')!; // ← #target が確実に存在している場合は ! 演算子でTS認識させるようにする
     target.append(container);
 
     // cpuのAPIデータを取得して option 要素に追加する
     await CpuSectionController.cpuBrandElements();
-    // cpuのBrandを切り替えたら同じブランドのgpuを取得する処理を登録する
-    InputChange.addEvent(document.getElementById(CpuViews.cpuBrandId), CpuSectionController.cpuModelElements);
-    // cpuのAPIデータを取得して option 要素に追加する
+    // cpu の Brand の seletc 要素を切り替えたら同じブランドの cpu を取得する処理を登録する
+    InputChange.addEvent(document.getElementById(CpuViews.cpuBrandId)!, CpuSectionController.cpuModelElements);
+    // cpu の APIデータを取得して option 要素に追加する
     await GpuSectionConttoller.gpuBrandElements();
-
+    // storage の APIデータを取得して option 要素に追加する
     await StorageSectionController.strageTypeElement();
 
-    InputChange.addEvent(document.getElementById(GpuViews.gpuBrandId), GpuSectionConttoller.gpuModelElements);
+    // gpuModel の select 要素を選択したとき発火するイベントの処理を追加
+    InputChange.addEvent(document.getElementById(GpuViews.gpuBrandId)!, GpuSectionConttoller.gpuModelElements);
 
     /* メモリー項目選択時のイベント */
     InputChange.addEvent(
-      document.getElementById(MemoryViews.memoryQuantityId),
+      document.getElementById(MemoryViews.memoryQuantityId)!,
       MemorySectionController.addMemoryBrandElements
     );
     InputChange.addEvent(
-      document.getElementById(MemoryViews.memoryBrandId),
+      document.getElementById(MemoryViews.memoryBrandId)!,
       MemorySectionController.addMemoryModelElements
     );
-    InputChange.addEvent(document.getElementById(MemoryViews.memoryModelId), MemorySectionController.addComputerEntity);
+    InputChange.addEvent(
+      document.getElementById(MemoryViews.memoryModelId)!,
+      MemorySectionController.addComputerEntity
+    );
     /***************************/
 
     InputChange.addEvent(
-      document.getElementById(StorageViews.storageTypeId),
+      document.getElementById(StorageViews.storageTypeId)!,
       StorageSectionController.storageSizeElements
     );
 
@@ -73,4 +73,8 @@ class App {
   }
 }
 
+/**
+ * トップレベルの 'await' 式は、'module' オプションが 'es2022'、'esnext'、'system'、'node16'　または 'nodenext' に設定されていて、
+ * 'target' オプションが 'es2017' 以上に設定されている場合にのみ使用できます。
+ */
 await App.createTopView();
