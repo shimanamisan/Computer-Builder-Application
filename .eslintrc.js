84b989bbd93fd21d4072c5849d2e1356a74ae0c5
shimanamisan <mikan.sup.all@gmail.com>
2023-01-09 14:39:37 +0900

    update: typescript ESLintを使用するための設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   // この階層よりも親階層のディレクトリを探しに行かなくなる
   root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   // 検証するJavaScriptの実行環境を指定する
   env: {
     // ブラウザで動作するJavaScriptなのかNode.jsで動作するJavaScriptなのか指定する
@@ -8,10 +10,9 @@ module.exports = {
     es2021: true, // es2020までの構文を利用してもエラーとならない
     node: true,
   },
-  // 新しい構文でも使用できるようにする
-  parser: '@babel/eslint-parser',
   // 外部で提供されるルールを適応させる
-  extends: ['eslint:recommended', 'prettier'], // ESLintが提供しているおすすめのルールを適応
+  // extends: ['airbnb-base', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   // ルールを指定する
   // extendsで指定したルールと重複した場合はこちらの設定が有効になる
   rules: {
@@ -19,8 +20,12 @@ module.exports = {
     'prefer-const': 'error', // 更新をしない変数の宣言にconst以外が指定されていたらエラーが発生するルール
     indent: ['error', 2],
     semi: ['error', 'always'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
   parserOptions: {
     sourceType: 'module', // import, export などのesmodulesの構文を利用してもエラーとならない
+    ecmaVersion: 'latest',
+    project: './tsconfig.eslint.json',
   },
 };
